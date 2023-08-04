f1c023292b2197f9c7c2b933af8cff63d9d490d8
whitenight1201 <whitenight1201021@gmail.com>
2023-08-04 06:05:50 -0700

    creating and using multi-module workspaces in Go

---
+++ hello/go.mod
@@ -0,0 +1,5 @@
+module example.com/hello
+
+go 1.20
+
+require golang.org/x/example/hello v1.1.0
---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
+func main() {
+	fmt.Println(reverse.String("Hello"), reverse.Int(24601))
+}
