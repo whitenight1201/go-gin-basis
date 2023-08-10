073f14d0da37db5edccca7d54eb81cfc8a6372f3
whitenight1201 <whitenight1201021@gmail.com>
2023-08-10 06:24:12 -0700

    create database collection

---
+++ Collection/getCollection.go
@@ -0,0 +1,12 @@
+package getcollection
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// This function gets the Collection from the MongoDB database.
+// The database name, in this case, is myGoappDB, with Posts as its collection.
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("myGoappDB").Collection("Posts")
+	return collection
+}
