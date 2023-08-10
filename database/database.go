55a110723380d2e2ae52816902121b7599885aa7
whitenight1201 <whitenight1201021@gmail.com>
2023-08-10 06:20:39 -0700

    connect Go to MonogDB

---
+++ database/database.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	Mongo_URL := "mongodb://127.0.0.1:27017"
+	client, err := mongo.NewClient(options.Client().ApplyURI(Mongo_URL))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	defer cancel()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to mongoDB")
+	return client
+}
