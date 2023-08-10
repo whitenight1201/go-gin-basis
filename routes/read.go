40b726edb9294b075ac7a82e11d64e8e83b4d6d4
whitenight1201 <whitenight1201021@gmail.com>
2023-08-10 07:32:23 -0700

    GET endpoints, read a single document from the db via its unique ID

---
+++ routes/read.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	getcollection "CRUD_API/Collection"
+	database "CRUD_API/database"
+	model "CRUD_API/model"
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func ReadOnePost(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var DB = database.ConnectDB()
+	var postCollection = getcollection.GetCollection(DB, "Posts")
+
+	//The postId variable is a parameter declaration. It gets a document's object ID as objId.
+	postId := c.Param("postId")
+	var result model.Post
+
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(postId)
+
+	err := postCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&result)
+
+	res := map[string]interface{}{"data": result}
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "success!", "Data": res})
+}
