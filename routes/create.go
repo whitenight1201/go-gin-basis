ac5e64599842b954c5b5b36f9552bbf08582ee6e
whitenight1201 <whitenight1201021@gmail.com>
2023-08-10 07:16:15 -0700

    defining the POST endpoint to write data into the database

---
+++ routes/create.go
@@ -0,0 +1,44 @@
+package routes
+
+import (
+	getcollection "CRUD_API/Collection"
+	database "CRUD_API/database"
+	model "CRUD_API/model"
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func CreatePost(c *gin.Context) {
+	var DB = database.ConnectDB()
+	var postCollection = getcollection.GetCollection(DB, "Posts")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	post := new(model.Post)
+	defer cancel()
+
+	// c.BindJSON("post") is a JSONified model instance that calls each model field as postPayload; this goes into the database.
+	if err := c.BindJSON(&post); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err})
+		log.Fatal(err)
+		return
+	}
+
+	postPayload := model.Post{
+		Id:      primitive.NewObjectID(),
+		Title:   post.Title,
+		Article: post.Article,
+	}
+
+	result, err := postCollection.InsertOne(ctx, postPayload)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Posted successfully", "Data": map[string]interface{}{"data": result}})
+}
