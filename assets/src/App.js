521d497fc0793a84d3e5dc1e417f809d2e505d4d
whitenight1201 <whitenight1201021@gmail.com>
2023-08-14 06:25:40 -0700

    Change assests to React frontend works well..

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+import PostsPage from './pages/PostsPage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authContext = useContext(AuthContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage/>
+        </Route>
+        {!authContext.loggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        <Route path="/posts">
+          {authContext.loggedIn && <PostsPage />}
+          {!authContext.loggedIn && <Redirect to="/auth" />}
+        </Route>
+        <Route path="*">
+          <Redirect to="/"/>
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
