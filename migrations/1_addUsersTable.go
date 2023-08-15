0feea9031936fff114800555e814c8aebfb40cbe
whitenight1201 <whitenight1201021@gmail.com>
2023-08-15 00:41:21 -0700

    migrations -- create tables in which our data will be store

---
+++ migrations/1_addUsersTable.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-pg/migrations/v8"
+)
+
+func init() {
+	migrations.MustRegisterTx(func(db migrations.DB) error {
+		fmt.Println("creating table users...")
+		_, err := db.Exec(`CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL,
+      created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+      modified_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    )`)
+		return err
+	}, func(db migrations.DB) error {
+		fmt.Println("dropping table users...")
+		_, err := db.Exec(`DROP TABLE users`)
+		return err
+	})
+}
